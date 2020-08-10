cb055d56374f13e1b17cede39c6fa2adae8e920a
Isaac Chang <ichang1@haverford.edu>
2020-08-09 23:16:02 -0400

    added snake and made it move with collision

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from components import Board
-from pygame import event
+from components import Board, Snake, Location
+from pygame import event, time
 from pygame.constants import (
     QUIT
     )
-import curses
 
 if __name__ == "__main__":
     width = 1200
     height = 900
     scale = 30
-    board = Board(1200, 900, 30)
+    startLength = 3
 
-    while True:
+    board = Board(1200, 900, 30)
+    snake = Snake(Location(20,15)) #initial with non scaled location
+
+    for i in range(startLength - 1):
+        snake.grow()
+
+    while (not snake.inBody() and not snake.inWall(board)):
         for e in event.get():
             if (e.type == QUIT) :
                 quit()
-        board.draw()
+        time.delay(50)
+        snake.move()
+        board.draw(snake)
